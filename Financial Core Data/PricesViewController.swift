4706c8b4c7d7a5f6304d48d86aad9f33f77226be
Warren Hansen <whansen1@mac.com>
2018-01-11 15:29:04 -0800

    task: tableview set up

--- Financial Core Data/PricesViewController.swift
+++ Financial Core Data/PricesViewController.swift
@@ -8,28 +8,30 @@
 
 import UIKit
 
-class PricesViewController: UIViewController {
+class PricesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableview: UITableView!
+    
+    let titleArray = ["AAPL", "GOOG", "MSFT", "SPY"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return titleArray.count
     }
-    */
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
+        // for custom cell
+        //let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as!StockTableViewCell
+        cell.textLabel?.text = titleArray[indexPath.row]
+        
+        return cell
+    }
 
 }
